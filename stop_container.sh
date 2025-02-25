3e74aa93c3e6e2643970dd7040584cec2812e491
Byreshkumar Billemane Revanna <BB00469674@techmahindra.com>
2025-02-25 13:25:19 +0530

    docker stop new

--- stop_container.sh
+++ stop_container.sh
@@ -3,6 +3,8 @@ set -e
 
 sudo su - root
 # Stop the running container (if any)
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
+#docker stop $(docker ps -a -q)
+#docker rm $(docker ps -a -q)
+containerid = `docker ps | awk -f " " '{print $1}'`
+docker rm -f $containerid
 echo "done"
