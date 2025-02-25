ea88835cf4c845dfb910d55f2af5595e6c0cd89c
byreshkumar <byreshk@gmail.com>
2025-02-25 11:10:29 +0000

    test push

--- stop_container.sh
+++ stop_container.sh
@@ -3,6 +3,9 @@ set -e
 
 sudo su - root
 # Stop the running container (if any)
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
-echo "done"
+#docker stop $(docker ps -a -q)
+#docker rm $(docker ps -a -q)
+
+containerid = `docker ps | awk -f " " '{print $1}'`
+docker rm -f $containerid
+echo "donewwwwwwwwwwwwwwwwwwwwwww"
